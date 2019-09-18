08d1b2b7158245f2521f6ad1eee23267eba18d81
NightFore <38656758+NightFore@users.noreply.github.com>
2019-09-18 22:44:12 +0200

    [Game Project 7] Adventurers of Elrualia v0.01c - Grid - Update(tile_list, grid_list, grid_size, grid_update).py

--- [Game Project 7] Adventurers of Elrualia.py
+++ [Game Project 7] Adventurers of Elrualia.py
@@ -562,7 +562,6 @@ tile_mountain_3 = load_tile_table("Data\Tilesets\Tile_mountain_3.png", 40, 40)
 tile_ocean      = load_tile_table("Data\Tilesets\Tile_ocean.png", 40, 40)
 tile_road       = load_tile_table("Data\Tilesets\Tile_road.png", 40, 40)
 tile_soil       = load_tile_table("Data\Tilesets\Tile_soil.png", 40, 40)
-tile_list       = [tile_woods, tile_desert, tile_grass, tile_mountain_1, tile_mountain_2, tile_mountain_3, tile_ocean, tile_road, tile_soil]
 
 ############################################################
 """
@@ -596,6 +595,10 @@ class MainIG():
         # List
         self.list_music     = load_file("Data\Music")
 
+        # Grid
+        self.tile_list      = [tile_woods, tile_desert, tile_grass, tile_mountain_1, tile_mountain_2, tile_mountain_3, tile_ocean, tile_road, tile_soil]
+        self.grid_size      = 40
+        self.grid_list      = [ [[0,0,0]]*int(display_width/self.grid_size) ] * int(display_height/self.grid_size)
 
     def update(self):
         if self.title == True:
@@ -650,16 +653,24 @@ class MainIG():
         elif init == False:
             self.grid_update()
 
-            for index, tileset in enumerate(tile_list):
+            for index, tileset in enumerate(self.tile_list):
                 for x, row in enumerate(tileset):
                     for y, tile in enumerate(row):
                         gameDisplay.blit(tile, ((x+index)*40, y*40))
 
     def grid_update(self):
-        size = 40
-        for col in range(int(display_width/size)):
-            for row in range(int(display_height/size)):
-                pygame.draw.rect(gameDisplay, (0, 0, 0), (size*col, size*row, size, size), 5)
+        for col in range(int(display_width/self.grid_size)):
+            for row in range(int(display_height/self.grid_size)):
+                pygame.draw.rect(gameDisplay, (0, 0, 0), (self.grid_size*col, self.grid_size*row, self.grid_size, self.grid_size), 5)
+
+        index_x, index_y = 0, 0
+        for tile_index in self.grid_list:
+            for tile in tile_index:
+                gameDisplay.blit(self.tile_list[tile[0]][tile[1]][tile[2]], (index_x*self.grid_size, index_y*self.grid_size))
+                index_x += 1
+            index_y += 1
+            index_x = 0
+        
 MainIG = MainIG()
 
 Main_Screen()
