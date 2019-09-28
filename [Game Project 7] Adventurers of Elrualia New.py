df4f818002178b6c6223d6876669c583e29fced0
NightFore <38656758+NightFore@users.noreply.github.com>
2019-09-29 00:56:58 +0200

    [Game Project 7] Adventurers of Elrualia v0.12f - Overhaul - Update(Vector)

--- [Game Project 7] Adventurers of Elrualia New.py
+++ [Game Project 7] Adventurers of Elrualia New.py
@@ -1,8 +1,8 @@
 import pygame
 import os
 import pytmx
-
 from pygame.locals import *
+vec = pygame.math.Vector2
 """
     Settings
 """
@@ -263,56 +263,54 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((TILESIZE, TILESIZE))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.vx = 0
-        self.vy = 0
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
 
     def get_keys(self):
-        self.vx, self.vy = 0, 0
+        self.vel = vec(0, 0)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vel.x = -PLAYER_SPEED
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.vx = +PLAYER_SPEED
+            self.vel.x = +PLAYER_SPEED
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.vy = -PLAYER_SPEED
+            self.vel.y = -PLAYER_SPEED
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.vy = +PLAYER_SPEED
+            self.vel.y = +PLAYER_SPEED
 
-        if self.vx != 0 and self.vy != 0:
-            self.vx *= 0.7071
-            self.vy *= 0.7071
+        if self.vel.x != 0 and self.vel.y != 0:
+            self.vel *= 0.7071
 
     def collide_with_walls(self, dir):
         if dir == "x":
             hits = pygame.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vx > 0:
-                    self.x = hits[0].rect.left - self.rect.width
-                if self.vx < 0:
-                    self.x = hits[0].rect.right
-                self.vx = 0
-                self.rect.x = self.x
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
             
         if dir == "y":
             hits = pygame.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.height
-                if self.vy < 0:
-                    self.y = hits[0].rect.bottom
-                self.vy = 0
-                self.rect.y = self.y
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
 
     def update(self):
         self.get_keys()
-        self.x += self.vx * self.game.dt
-        self.y += self.vy * self.game.dt
-        self.rect.x = self.x
+        self.pos += self.vel * self.game.dt
+        self.rect.x = self.pos.x
         self.collide_with_walls("x")
-        self.rect.y = self.y
+        self.rect.y = self.pos.y
         self.collide_with_walls("y")
+
+
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, game, x ,y):
